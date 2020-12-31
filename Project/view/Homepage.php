779760e163998a6f4dc3ffd0fb8372774e316503
SunanRashidAIUB <sunanrashid@gmail.com>
2020-12-31 22:27:14 +0600

    Created 3 folers
    Added hompage button

---
+++ Project/view/Homepage.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+	<title>garments ecommerce</title>
+	<style>
+		body{
+			background-color: #F0F2F5;
+		}
+	</style>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+  <div style="display:inline-block;">
+    <?php include 'header.php' ?>
+  </div>
+	<header>
+		<div class="logo">
+			<img src="./Textile.jpg" alt="Logo" style="width:300px;height:150px" >
+		</div>
+		
+		<h2> Garments E-commerce </h2>
+			<nav>
+			<ul>
+				<li class="active">
+					<a href="#">Home</a>
+				</li>
+				<li class="tive">
+					<a href="#">About</a>
+				</li>
+				<li class="Signin">
+					<a href="AdminSignin.php">Sign In</a>
+				
+			</ul>
+			</nav>
+		
+	</header>
+  <div>
+    <?php include 'footer.php' ?>
+  </div>
+	
+</body>
+</html>
