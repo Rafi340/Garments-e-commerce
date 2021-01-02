fbb3108b7bb098b94fb04bbedd01fb080ede21a8
SunanRashidAIUB <sunanrashid@gmail.com>
2021-01-02 16:40:38 +0600

    Update a_login.php

--- Project/controller/a_login.php
+++ Project/controller/a_login.php
@@ -81,11 +81,11 @@ if(isset($error_message))
      <table>
 	<tr>
 		<td>Username: </td>
-		<td><input type="text" name="username"></td>
+		<td><input type="text" name="username" required></td>
 	</tr>
 	<tr>
 		<td>Password: </td>
-		<td><input type="password" name="password"></td>
+		<td><input type="password" name="password" required></td>
 	</tr>
 	<tr>
 		<td></td>
