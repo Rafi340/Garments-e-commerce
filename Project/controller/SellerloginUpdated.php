b5a7586bb933fa5d48176ab506e8c45bb34c5d9a
AsAyeshaSiddika <ashua21221@gmail.com>
2021-01-03 00:14:40 +0600

    updated login
    login page validation done

--- Project/controller/SellerloginUpdated.php
+++ Project/controller/SellerloginUpdated.php
@@ -5,20 +5,8 @@
 </head>
 <body>
  
-    <?php include 'header.php' ?>
   
-
-	
-	    
-	<?php 
-	$cookie_name = "ayesha"; 
-	$cookie_value = "1"; 
-	setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); 
-	$myfile = fopen("output/cookies.txt", "w") or die("Unable to open file!"); 
-	fwrite($myfile, $cookie_name . " " . $cookie_value . " "); 
-	fclose($myfile); 
-	?>	
-
+  
 		
 		
 		 <?php
@@ -53,10 +41,10 @@
 	
 	<form method="POST" action=""<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
     User Name:<input type="text='0'" name="name" Required>
-    <span><?php echo $nameError; ?></span><br />
+
     <br><br>
     Password:<input type="var" name="password" Required>
-    <?php echo $passError; ?><br>
+    
     <br><br>
     <input type="submit" value="Login">
     
