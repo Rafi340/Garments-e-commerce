60976ed4596b14cacc0d98cbe001ba92a3c34aae
AsAyeshaSiddika <ashua21221@gmail.com>
2021-01-03 00:30:29 +0600

    Acvievement of Seller
    This is Seller Achievement Page.

---
+++ Project/controller/SellerAchievement.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body  bgcolor="sky blue">
+<form action="" method ="post">
+<img src="image/tex26.jpg" alt="" /><img src="image/tex25.jpg" alt="" /><img src="image/tex27.jpg" alt="" /></br></br>
+<b><i>Our Achievements</i></b>:<input type="text" style="background-color:yellow "  name="search"/>
+</br>
+</br>
+</br>
+	<input type="submit" name="submit" value="Submit" style="background-color:green" method="post" />
+	</br>
+	</br></form>
+	<a href="UserView.php"><button style="background-color:red">Back</button><a>
+</body>
+</html>
