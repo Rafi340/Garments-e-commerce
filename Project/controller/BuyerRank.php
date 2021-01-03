a29f112671a513f356ff66831fb6305189a11823
AsAyeshaSiddika <ashua21221@gmail.com>
2021-01-03 08:59:44 +0600

    Rank Of Buyer
    In Seller Part Ranking Of Buyer Is Done

---
+++ Project/controller/BuyerRank.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body  bgcolor="yellow">
+	<h2><big>Welcome to our buyer rank procedure</big></h2>
+	<h3><i>We Rank Our Customers Following This:</i></h3>
+	<img width="1000px" height="600px" src="image/tex20.jpg" alt="" />
+
+	<form action="" method ="post">
+    <div class="absolute9"><br><b><i>When Purchase Week is-"3" &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Amount Spent is-"$54.75" &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Customer Rank Point is-"54.00"</i></b></div> 
+<div class="absolute10"><br><b><i>When Purchase Week is-"10"   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Amount Spent is-"$15.25"  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Customer Rank Point-"15.25"</i></b></div> 
+<div class="absolute11"><br><b><i>When Purchase Week is-"15"   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Amount Spent is-"$35.00"   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Customer Rank Point-"35.00"</i></b></div> 
+<div class="absolute10"><br><b><i>When Purchase Week is-"25"    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Amount Spent is-"$107.50"   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Customer Rank Point-"107.50"</i></b></div> 	
+<div class="absolute10"><br><b><i>When Purchase Week is-"34"    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Amount Spent is-"$20.00"  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Customer Rank Point-"20.00"</i></b></div>
+<div class="absolute10"><br><b><i>When Purchase Week is-"41"     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Amount Spent is-"$10.75"    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Customer Rank Point-"10.75"</i></b></div> </br></br></br>
+<div class="absolute10"><br><b>So, Total Week is-"52"      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Total point is-"$379.00"   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;    Avarage Customer Rank Point is-"15.25"</b></div>  
+    </div>
+		  </form></br></br></br></br></br>
+		  <a href="UserView.php"><button style="background-color:red">Back</button><a>
+</body>
+</html>
