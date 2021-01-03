d07e965002a0464da088c55d81f03e773fb339ff
AsAyeshaSiddika <ashua21221@gmail.com>
2021-01-04 05:26:05 +0600

    Sellers Top Product

---
+++ Project/controller/topProduct.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body  bgcolor="#ccff99">
+    <center>
+	<h2><big><i>Our Top Product List</big></i></h2>
+	<h2><i>Girls:</i></h2>
+	<select style="background-color:#ff4000" name="" id="">
+		<option value="">Shari</option>
+		<option value="">Kamiz</option>
+		<option value="">Orna</option>
+	</select>
+	<img src="../image/n.jpg" alt="" /><br><br><br>
+	<h2><i>Boys:</i></h2>
+	<select style="background-color:#ff4000" name="" id="">
+		<option value="">Shirt</option>
+		<option value="">Pant</option>
+		<option value="">Jacket</option>
+	</select>
+	<img src="../image/n1.jpg" alt="" />
+	<img width="300px" height="200px" src="../image/n2.jpg" alt="" />
+	<br><br><br><br><br>
+	
+	<h2><i>Children:</i></h2>
+	<select style="background-color:#ff4000" name="" id="">
+		<option value="">Dress</option>
+		<option value="">shoe</option>
+	</select>
+	<img src="../image/n4.jpg" alt="" /><br><br><br><br><br>
+	<h2><i>Home Accessories:</i></h2>
+	<select style="background-color:#ff4000" name="" id="">
+		<option value="">Sofa</option>
+		<option value="">Cover</option>
+	</select>
+	<img src="../image/n5.jpg" alt="" /><br><br><br><br><br>
+	<a href="UserView.php"><button style="background-color:red">Back</button><a>
+	</center>
+</body>
+</html>
