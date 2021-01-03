a7d16358993017837b61f07e1665077da251171a
AsAyeshaSiddika <ashua21221@gmail.com>
2021-01-04 04:46:04 +0600

    Updated Seller Profile Using CSS
    updated

---
+++ Project/view/myProfile.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<link rel="stylesheet" href="../css/log.css" />
+</head>
+<body>
+	<div class="profile">
+	<center>
+	<h2><big><i>Welcome to My profile</big></i></h2>
+	<img width="" height="" src="image/t1.png" alt=""/>
+	<img width="200px" height="200px" src="../image/tex32.jpg" alt=""/>
+	<h3>Name: Ayesha Siddika</h3>
+	<h3>User ID: 17-9872-1 </h3>
+	<h3>User Type: Seller</h3>
+	<h3>Gender: Female</h3>
+	<h3>Date of Birth: 11-02-1997</h3> </br>
+	<a href="UserView.php"><button style="background-color:red">Back</button><a>
+	</div>
+</body>
+</html>
