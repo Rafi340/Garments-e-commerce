784603a43a4c3241a0c14a7a5167c186289f93ca
AsAyeshaSiddika <ashua21221@gmail.com>
2021-01-04 05:56:12 +0600

    View Order On Seller

---
+++ Project/controller/ViewOrder.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body  bgcolor="sky blue">
+<form action="" method ="post">
+    <div class="absolute9"><br>For Ladies Section Per Day Order- 10,000</br>
+	Categories- Sari,Kurti,Hizab,Kamiz </br></div> 
+	    </div>
+		</br>
+		<div class="absolute10"><br>For Gents Section Per Day Order- 20,000</br>
+	Categories- Shirts,Pants,Panjabi </br></div> 
+	    </div>
+		</br>
+		<div class="absolute11"><br>For Babies Section Per Day Order- 15,000</br>
+	Categories- Woolen Clothes,Tupi </br></div> 
+	    </div>
+		</br>
+		</br>
+		<div class="absolute12"><br>Average Total Order Per Day- 90,000</br>
+	</div> 
+	    </div>
+		  </form>
+		  </br>
+		  </br>
+		  </br>
+		  </br>
+		  </br>
+		  </br>
+		  </br>
+		  </br>
+		  </br>
+		  </br>
+		  <a href="UserView.php"><button style="background-color:red">Back</button><a>
+</body>
+</html>
