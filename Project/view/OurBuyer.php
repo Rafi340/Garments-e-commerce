d8854b9f956f98e6cf1662246d5f7ab4fbf0d12e
AsAyeshaSiddika <ashua21221@gmail.com>
2021-01-04 04:51:02 +0600

    Updated Our Buyer In Seller Part
    updated

---
+++ Project/view/OurBuyer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body  bgcolor="pink">
+<form action="" method ="post">
+	<h2><big><i>About our buyer</i></big></h2>
+	<img width="350px" height="250px" src="../image/tex28.jpg" alt="" /><img width="350px" height="250px" src="../image/tex29.jpg" alt="" />
+    <div class="absolute9"><br><b><i>Customers are the individuals and businesses that purchase goods and services from another business.To understand how to better meet the needs of its customers, some businesses closely monitor their customer relationships to identify ways to improve service and products.The way businesses treat their customers can give them a competitive edge.Businesses often honor the adage "the customer is always right" because happy customers are more likely to award repeat business to companies who meet or exceed their needs. As a result, many companies closely monitor their customer relationships to solicit feedback on methods to improve product lines. Customers are categorized in many ways. Most commonly, customers are classified as external or internal.
+External customers are dissociated from business operations and are often the parties interested in purchasing the final goods and services produced by a company. Internal customers are individuals or businesses integrated into business operations, often existing as employees or other functional groups within the company.</b></i></br></div> 
+	    </div></form></br></br></br></br></br></br></br>
+		<a href="UserView.php"><button style="background-color:red">Back</button><a>
+</body>
+</html>
